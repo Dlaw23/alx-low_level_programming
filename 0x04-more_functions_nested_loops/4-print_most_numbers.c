d13a8b9cf1aeb2569c79380074def055263720cc
sunday udhawuve <udhawuvelawrence@gmail.com>
2022-12-15 14:19:37 -0800

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints number
- */
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
+*/
 
 void print_most_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 48; i < 58; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (i != 50 && i != 52)
-		{
-			_putchar(i);
-		}
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
