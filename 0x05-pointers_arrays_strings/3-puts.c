a797c5959a360c6ec78cb967afc2711eb9344a03
sunday udhawuve <udhawuvelawrence@gmail.com>
2022-12-19 14:43:57 -0800

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,14 +6,14 @@
  * Return: the lenght of the string
  */
 
-void _puts(char *str);
+void _puts(char *str)
 {
 	int a = 0;
 
 	while (str[a] != '\0')
 	{
-		_putchar(str[a])
-			a++;
+		_putchar(str[a]);
+		a++;
 	}
 	_putchar('\n');
 }
